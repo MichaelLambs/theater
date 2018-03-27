5d732be4334407b195337ba1c5d97e8c17134c25
Michael Lamb <503lamb@gmail.com>
2018-03-26 21:19:34 -0600

    finished class homework

--- Models/BoxOffice.cs
+++ Models/BoxOffice.cs
@@ -1,21 +1,47 @@
+using System;
 using System.Collections.Generic;
 using theater.Assets.Enums;
 
 namespace theater.Models
 {
-  public class BoxOffice
-  {
-    public List<Movie> Movies = new List<Movie>();
-
-    public void SetupBoxOffice()
+    public class BoxOffice
     {
-      Director ryan = new Director() { Name = "Ryan Coogler" };
-      var bp = new Movie() { Title = "Black Panther", Runtime = 90, Rating = Ratings.PG13, Director = ryan };
-      var creed = new Movie() { Title = "Creed", Runtime = 90, Rating = Ratings.PG13, Director = ryan };
-      Movies.Add(bp);
-      Movies.Add(creed);
+        public List<Movie> Movies = new List<Movie>();
+
+        public Movie selectedMovie { get; set; }
+
+        public void SetupBoxOffice()
+        {
+            Director ryan = new Director() { Name = "Ryan Coogler" };
+            var bp = new Movie() { Title = "Black Panther", Runtime = 90, Rating = Ratings.PG13, Director = ryan };
+            var creed = new Movie() { Title = "Creed", Runtime = 90, Rating = Ratings.PG13, Director = ryan };
+            Movies.Add(bp);
+            Movies.Add(creed);
+        }
+
+        public void ShowMovies()
+        {
+            int choice = -1;
+            while (choice == -1)
+            {
+                var i = 1;
+                Movies.ForEach(movie =>
+                {
+                    System.Console.WriteLine($@"
+          -------------------------------------
+          |{i}. {movie.Title} | {movie.Runtime} | {movie.Rating}
+          -------------------------------------
+          ");
+                    i++;
+                });
+                System.Console.WriteLine("Pick a Movie to Add");
+                var userInput = Console.ReadLine();
+                int.TryParse(userInput, out choice);
+            }
+            selectedMovie = Movies[choice - 1];
+            System.Console.WriteLine($"You are adding {selectedMovie.Title}");
+        }
+
+
     }
-
-
-  }
 }
--- Models/Cinema.cs
+++ Models/Cinema.cs
@@ -3,104 +3,127 @@ using System.Collections.Generic;
 
 namespace theater.Models
 {
-  public class Cinema
-  {
-    public string Name { get; set; }
-    public Address Address { get; set; }
-    public double DefaultTicketPrice { get; set; }
-    public List<Theater> Theaters { get; set; } = new List<Theater>();
-    public List<Showtime> Showtimes { get; set; } = new List<Showtime>();
-
-
-    public void CreateTheaterDetails()
+    public class Cinema
     {
-      System.Console.WriteLine("Theater Name: ");
-      var name = Console.ReadLine();
-      int rn = -1;
-      while (rn < 0)
-      {
-        System.Console.WriteLine("Theater Number: ");
-        var roomNumber = Console.ReadLine();
-        int.TryParse(roomNumber, out rn);
-        Console.Clear();
-      }
-      int rc = -1;
-      while (rc < 0)
-      {
-        System.Console.WriteLine("Theater Capacity: ");
-        var capacity = Console.ReadLine();
-        int.TryParse(capacity, out rc);
-        Console.Clear();
-      }
-      CreateTheater(name, rn, rc);
-    }
-
-    public void CreateTheater(string name, int roomNumber, int capacity)
-    {
-      Theaters.Add(new Theater()
-      {
-        Name = name,
-        RoomNumber = roomNumber,
-        Capacity = capacity
-      });
-    }
+        public string Name { get; set; }
+        public Address Address { get; set; }
+        public double DefaultTicketPrice { get; set; }
+        public List<Theater> Theaters { get; set; } = new List<Theater>();
+        public List<Showtime> Showtimes { get; set; } = new List<Showtime>();
+        public Theater activeTheater { get; set; }
 
 
-    public void PrintShowtimes()
-    {
-      Showtimes.ForEach(showtime =>
+        public void CreateTheaterDetails()
         {
-          System.Console.WriteLine($@"
+            System.Console.WriteLine("Theater Name: ");
+            var name = Console.ReadLine();
+            int rn = -1;
+            while (rn < 0)
+            {
+                System.Console.WriteLine("Theater Number: ");
+                var roomNumber = Console.ReadLine();
+                int.TryParse(roomNumber, out rn);
+            }
+            int rc = -1;
+            while (rc < 0)
+            {
+                System.Console.WriteLine("Theater Capacity: ");
+                var capacity = Console.ReadLine();
+                int.TryParse(capacity, out rc);
+                Console.Clear();
+            }
+            CreateTheater(name, rn, rc);
+        }
+
+        public void ShowTheaters()
+        {
+            Console.Clear();
+            int choice = -1;
+            while (choice == -1)
+            {
+                System.Console.WriteLine("Pick a theater");
+                var i = 1;
+                Theaters.ForEach(theater =>
+                {
+                    System.Console.WriteLine($@"
+        ----------------------------
+        |{i}. {theater.Name} | {theater.RoomNumber}
+        ----------------------------
+            ");
+                    i++;
+                });
+                System.Console.WriteLine("Which theater do you want to pick?");
+                var userInput = Console.ReadLine();
+                int.TryParse(userInput, out choice);
+            }
+            activeTheater = Theaters[choice - 1];
+            System.Console.WriteLine($"You have selected {activeTheater.Name}");
+        }
+        public void CreateTheater(string name, int roomNumber, int capacity)
+        {
+            Theaters.Add(new Theater()
+            {
+                Name = name,
+                RoomNumber = roomNumber,
+                Capacity = capacity
+            });
+        }
+
+        public void PrintShowtimes()
+        {
+            Showtimes.ForEach(showtime =>
+              {
+                  System.Console.WriteLine($@"
         -----------------------------------------------------
         | {showtime.Movie.Title} | {showtime.Time} |
         ------------------------------------------------------
             ");
-        });
-    }
+              });
+        }
 
-    public void ShowAvailableTickets(Showtime showtime)
-    {
-      System.Console.WriteLine($@"
+        public void ShowAvailableTickets(Showtime showtime)
+        {
+            System.Console.WriteLine($@"
 -----------------------------------------------------
 | Price | Seat |
 ------------------------------------------------------");
-      showtime.Theater.Tickets.ForEach(ticket =>
-      {
-        if (!ticket.Purchased)
-        {
-          System.Console.WriteLine($@"
+            showtime.Theater.Tickets.ForEach(ticket =>
+            {
+                if (!ticket.Purchased)
+                {
+                    System.Console.WriteLine($@"
 ${ticket.Price} | {ticket.SeatNumber}");
+                }
+            });
         }
-      });
-    }
 
-    public void CreateShowtime(Theater theater, Movie movie, string time)
-    {
-      var newShowtime = new Showtime()
-      {
-        Cinema = this,
-        Theater = theater,
-        Movie = movie,
-        Time = time
-      };
-      Showtimes.Add(newShowtime);
-      MakeTickets(newShowtime);
-    }
-
-    private void MakeTickets(Showtime showtime)
-    {
-      for (var i = 0; i < showtime.Theater.Capacity; i++)
-      {
-        showtime.Theater.Tickets.Add(new Ticket()
+        public void CreateShowtime(Theater theater, Movie movie, string time)
         {
-          Showtime = showtime,
-          Price = DefaultTicketPrice,
-          SeatNumber = i + 1
-        });
-      }
+            var newShowtime = new Showtime()
+            {
+                Cinema = this,
+                Theater = theater,
+                Movie = movie,
+                Time = time
+            };
+            Showtimes.Add(newShowtime);
+            MakeTickets(newShowtime);
+        }
+
+        private void MakeTickets(Showtime showtime)
+        {
+            for (var i = 0; i < showtime.Theater.Capacity; i++)
+            {
+                showtime.Theater.Tickets.Add(new Ticket()
+                {
+                    Showtime = showtime,
+                    Price = DefaultTicketPrice,
+                    SeatNumber = i + 1
+                });
+            }
+        }
+
+
+
     }
-
-
-
-  }
 }
